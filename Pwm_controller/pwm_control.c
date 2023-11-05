04c7e736184b69f0932f13a93c259153a9a84b02
Karlis Tregers <93842312+Ka-rlis@users.noreply.github.com>
2023-11-05 14:31:25 +0100

    Update pwm_control.c

--- Pwm_controller/pwm_control.c
+++ Pwm_controller/pwm_control.c
@@ -14,9 +14,6 @@ void UpdatePWMFrequency(TIM_HandleTypeDef *htim, uint32_t desired_pwm_freq) {
     uint32_t prescaler = 0;
     uint32_t period = 0;
 
-    // Calculate prescaler and period for the desired frequency
-    // This is a simple example and might not give exact results due to integer division
-    // You will need to refine the calculation to handle rounding and ensure the values are within valid ranges
     prescaler = (timer_clock / (desired_pwm_freq * (htim->Init.Period + 1))) - 1;
     period = (timer_clock / (desired_pwm_freq * (prescaler + 1))) - 1;
 
@@ -31,8 +28,6 @@ void UpdatePWMFrequency(TIM_HandleTypeDef *htim, uint32_t desired_pwm_freq) {
     if (HAL_TIM_PWM_Init(htim) != HAL_OK) {
         Error_Handler();
     }
-
-    // If needed, reconfigure the PWM channel
     TIM_OC_InitTypeDef sConfigOC = {0};
     sConfigOC.OCMode = TIM_OCMODE_PWM1;
     sConfigOC.Pulse = period / 2; // 50% duty cycle
@@ -42,15 +37,12 @@ void UpdatePWMFrequency(TIM_HandleTypeDef *htim, uint32_t desired_pwm_freq) {
         Error_Handler();
     }
 
-    // Start PWM
+
     if (HAL_TIM_PWM_Start(htim, TIM_CHANNEL_1) != HAL_OK) {
         Error_Handler();
     }
 }
 
-// ... (other code)
-
-// Example of how to call the function to update frequency
 void SetNewPWMFrequency() {
     uint32_t new_frequency = 10000; // Set the new frequency to 10 kHz
     UpdatePWMFrequency(&htim14, new_frequency);
