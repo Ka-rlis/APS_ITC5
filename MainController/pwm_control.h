e55d5291be351421c20e70e940fda0453af1720e
Karlis Tregers <93842312+Ka-rlis@users.noreply.github.com>
2023-11-23 14:50:53 +0100

    Update pwm_control.h

--- MainController/pwm_control.h
+++ MainController/pwm_control.h
@@ -1,10 +1,15 @@
 // pwm_control.h
+
 #include "stm32f4xx_hal.h"
 #ifndef PWM_CONTROL_H
 #define PWM_CONTROL_H
 
+extern volatile int32_t angular_position_z;
+int32_t PWM_GyroMapping(int32_t angular_position_z);
+float Gyro_Data_Integration(int32_t gyroZAxisValue);
 
-uint32_t PWM_GyroMapping(uint32_t gyroZAxisValue);
+
+
 
 #endif // PWM_CONTROL_H
 
