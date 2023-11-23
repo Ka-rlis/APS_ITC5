5d1b1ddeabd1f9bdb29849aee2f3c8cfa1b95d3e
Karlis Tregers <93842312+Ka-rlis@users.noreply.github.com>
2023-11-23 10:03:25 +0100

    Update pwm_control.h

--- Pwm_controller/pwm_control.h
+++ Pwm_controller/pwm_control.h
@@ -1,10 +1,13 @@
 // pwm_control.h
+#include "stm32f4xx_hal.h"
 #ifndef PWM_CONTROL_H
 #define PWM_CONTROL_H
+#define PWM_PERIOD_TICKS 20000
+#define PWM_MIN 850
+#define PWM_MAX 2150
 
-#include "stm32f4xx_hal.h"
-// Include the TIM HAL header
 void UpdatePWMFrequency(TIM_HandleTypeDef *htim, uint32_t desired_pwm_freq);
-
+uint32_t MapZAxisToPWMDutyCycle(float z_axis_value);
+void UpdatePWMDutyCycle(TIM_HandleTypeDef *htim, uint32_t channel, float z_axis_value);
 #endif // PWM_CONTROL_H
 
