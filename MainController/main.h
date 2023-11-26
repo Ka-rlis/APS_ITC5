a31a5e63851dd76bca7c1e686a27eca2eeba5176
Karlis Tregers <93842312+Ka-rlis@users.noreply.github.com>
2023-11-26 20:31:09 +0100

    Update main.h

--- MainController/main.h
+++ MainController/main.h
@@ -61,10 +61,6 @@ void MX_TIM3_Init(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define USART_TX_Pin GPIO_PIN_2
-#define USART_TX_GPIO_Port GPIOA
-#define USART_RX_Pin GPIO_PIN_3
-#define USART_RX_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
