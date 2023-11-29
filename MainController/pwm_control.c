3a129a4a704d654a6f1eef6f4224f9e120f1a29b
Karlis Tregers <93842312+Ka-rlis@users.noreply.github.com>
2023-11-29 14:04:48 +0100

    Update pwm_control.c

--- MainController/pwm_control.c
+++ MainController/pwm_control.c
@@ -4,35 +4,42 @@
 #include "main.h"
 #include "app_mems.h"
 #include "pwm_control.h"
+#include "motion_fx.h"
 
-
-int32_t gyroZAxisValue;
 int current_duty_cycle = 1500;
-volatile int32_t angular_position_z;
+int gyroZAxisValue = 0;
 
-float Gyro_Data_Integration(int32_t gyroZAxisValue){
+
+int get_data(MFX_input_t data_in){
+	gyroZAxisValue = data_in.gyro[2];
+
+	return gyroZAxisValue;
+}
+
+float Gyro_Data_Integration(gyroZAxisValue){
+
 	uint32_t currentTime = HAL_GetTick();
 	uint32_t lastUpdateTime = 0;
+	float angular_position_z = 0;
 
 	float deltaTime = (currentTime - lastUpdateTime) / 1000.0f;
 	angular_position_z += (gyroZAxisValue * deltaTime);
 	lastUpdateTime = currentTime;
 
 	return angular_position_z;
-
 }
 
-void PWM_GyroMapping(int32_t gyroZAxisValue){
+int PWM_GyroMapping(int gyroZAxisValue){
 	// Min Pulse width = 0micros, Max = 20000micros
 	// Min motor PWM 900micros, Max = 2100micros
 	current_duty_cycle = (Gyro_Data_Integration(gyroZAxisValue) / 5000) + 1500;
 
     if (current_duty_cycle > 1700) {
-        htim2.Instance->CCR1 = 1700;
+        return 1700;
     } else if (current_duty_cycle < 1300) {
-        htim2.Instance->CCR1 = 1300;
+        return 1300;
     } else {
-    	htim2.Instance->CCR1 = current_duty_cycle;
+    	return current_duty_cycle;
     }
 }
 
