6c713e9be89a168ae74707338ad17fe14253b370
AdamsAliGills <116809315+AdamsAliGills@users.noreply.github.com>
2023-12-04 13:58:43 +0100

    Update complementary_filter.ino

--- complementary_filter.ino
+++ complementary_filter.ino
@@ -17,6 +17,7 @@ LSM303AGR_ACC_Sensor Acc2(&DEV_I2C);
 LSM303AGR_MAG_Sensor Mag(&DEV_I2C);
 
 
+
 // pi
 #define PI 3.1415926535897932384626433832795
 
@@ -53,9 +54,10 @@ int32_t gyroscope[3];
 float deltaTime;
 unsigned long lastTime;
 
-Matrix<2,1,float> Sensor_angle_Gyro = { 
+Matrix<3,1,float> Sensor_angle_Gyro = { 
     0, 
     0,
+    0
 };
 
 
@@ -68,6 +70,15 @@ Matrix<3,1,float> Sensor_Acc = {
     0, 
     0,
     0,
+};
+
+Matrix<2,1,float> Sensor_Mag = { 
+    0, 
+    0,
+};
+
+Matrix<1, 1, float> Sensor_angle_Mag{
+  0,
 };
 
 /*
@@ -97,12 +108,14 @@ Matrix<2, 2, float> L = {
 };
 */
 
-Matrix<2, 1, float> x_est{
+Matrix<3, 1, float> x_est{
+  0,
   0,
   0
 };
 
-Matrix<2, 1, float> x_est_next{
+Matrix<3, 1, float> x_est_next{
+  0,
   0,
   0
 };
@@ -114,32 +127,49 @@ void sensor_all(){
   // Get sensor data in mdps and mg
   AccGyr.Get_X_Axes(accelerometer);
   AccGyr.Get_G_Axes(gyroscope);
+    // Read magnetometer LSM303AGR.
+  int32_t magnetometer[3];
+  Mag.GetAxes(magnetometer);
 
-  // Get raw sensor data as matrix in rad/s and m/s squared
+  // Get raw sensor data as matrix in rad
   Sensor_angle_Gyro(0, 0) += (gyroscope[0] * mddps_to_rads) * deltaTime;
   Sensor_angle_Gyro(1, 0) += (gyroscope[1] * mddps_to_rads) * deltaTime;
-  
+  Sensor_angle_Gyro(2, 0) += (gyroscope[2] * mddps_to_rads) * deltaTime;
 
+  // Get sensor data as matrix in rad/s squared
   Sensor_Acc(0, 0) = accelerometer[0] * mg_to_si;
-  Sensor_Acc(1, 0) = accelerometer[0] * mg_to_si;
-  Sensor_Acc(2, 0) = accelerometer[0] * mg_to_si;
+  Sensor_Acc(1, 0) = accelerometer[1] * mg_to_si;
+  Sensor_Acc(2, 0) = accelerometer[2] * mg_to_si;
+
+  // magnetometer data
+  Sensor_Mag(0, 0) = magnetometer[0];
+  Sensor_Mag(1, 0) = magnetometer[1];
+
+
+
+
+  // roll n pitch acc
+  Sensor_angle_Acc(0, 0) = atan2(Sensor_Acc(0, 0),Sensor_Acc(1, 0));
+  Sensor_angle_Acc(1, 0) = atan2(Sensor_Acc(2, 0), g);
+
+  // yaw mag netto
+  Sensor_angle_Mag(0, 0) =  atan2(Sensor_Mag(0, 0),Sensor_Mag(1, 0));
 
 };
 
 
 void ob() { 
-  Sensor_angle_Acc(0, 0) = atan2(Sensor_Acc(0, 0),Sensor_Acc(1, 0) );
-  Sensor_angle_Acc(1, 0) = atan2(Sensor_Acc(2, 0), g);
 
 
-  x_est_next(1, 0) = alpha * (x_est(1, 0) + Sensor_angle_Gyro(1, 0)) + (1 - alpha) * Sensor_angle_Acc(1, 0);
+  //x_est_next(1, 0) = alpha * (x_est(1, 0) + Sensor_angle_Gyro(1, 0)) + (1 - alpha) * Sensor_angle_Acc(1, 0);
+  x_est_next(2, 0) = alpha * (x_est(2, 0) + Sensor_angle_Gyro(2, 0)) + (1 - alpha) * Sensor_angle_Mag(0, 0);
 
   // Compute x_est_next = A_obs * x_est + B_obs * [u; y_meas]
   //BLA::Matrix<2, 1> x_est_next = (A * x_est) + (Sensor_angle_Gyro)+ L*(Sensor_angle_Gyro - Sensor_angle_Acc);
 
   // Update the x_est to the current calc
-  x_est(1, 0) = x_est_next(1, 0);
-  Serial.println(x_est_next(1, 0));
+  x_est(2, 0) = x_est_next(2, 0);
+  Serial.println(x_est_next(2, 0));
 };
 
 
